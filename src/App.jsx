76af1e689ae714d4ea3c61c4a163858e4bc3624e
Ekarahma148 <ekaarahma010@gmail.com>
2024-10-23 12:19:26 +0700

    update 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Book from "./Components/Book";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
 
 function App(){
 return(
   <>
+  <Header/>
   <Book/>
+  <Footer/>
   </>
 );
 }
--- src/Components/Book.jsx
+++ src/Components/Book.jsx
@@ -1,10 +1,10 @@
 import { InfoIcon } from "lucide-react";
+import { Search } from "lucide-react";
 import { HeartIcon } from "lucide-react";
 import { useState } from "react";
 
 function Book(){
-    const [info,setInfo]=useState("");
-    const books=[
+  const books=[
         {
           id:1, 
           judul:"Bulan",
@@ -14,7 +14,7 @@ function Book(){
           Tahun_terbit:2015,
           Halaman:396
         },
-          {
+        {
           id:2,
           judul:"Ayah",
           image:"src/assets/ayah.jpeg",
@@ -24,48 +24,108 @@ function Book(){
           Halaman:412
         },
         {
-            id:3,
-            judul:"Hujan",
-            image:"src/assets/hujan.jpeg",
-            Pengarang:"Tere Liye",
-            Penerbit:"PT Gramedia Pustaka Umum",
-            Tahun_terbit:2016,
-            Halaman:320
-          },
-          {
-            id:4,
-            judul:"Saat-saat Jauh",
-            image:"src/assets/saat.jpeg",
-            Pengarang:"Lia Seplia",
-            Penerbit:"PT Gramedia Pustaka Umum",
-            Tahun_terbit:2021,
-            Halaman:280
-          },
-          {
-            id:5,
-            judul:"Pulang - Pergi",
-            image:"src/assets/pp.jpeg",
-            Pengarang:"Tere Liye",
-            Penerbit:"Sabak Grip Nusantara",
-            Tahun_terbit:2021,
-            Halaman:417
-          },
-      
+          id:3,
+          judul:"Hujan",
+          image:"src/assets/hujan.jpeg",
+          Pengarang:"Tere Liye",
+          Penerbit:"PT Gramedia Pustaka Umum",
+          Tahun_terbit:2016,
+          Halaman:320
+        },
+        {
+          id:4,
+          judul:"Saat-saat Jauh",
+          image:"src/assets/saat.jpeg",
+          Pengarang:"Lia Seplia",
+          Penerbit:"PT Gramedia Pustaka Umum",
+          Tahun_terbit:2021,
+          Halaman:280
+        },
+        {
+          id:5,
+          judul:"Pulang - Pergi",
+          image:"src/assets/pp.jpeg",
+          Pengarang:"Tere Liye",
+          Penerbit:"Sabak Grip Nusantara",
+          Tahun_terbit:2021,
+          Halaman:417
+        },
+        
       ]
-      function HandleInfo(){
+      const [liked,setLiked]=useState({});
+      const [info,setInfo]=useState("");
+      const [search,setSearch]=useState("");
 
+      const handleLike = (id) => {
+        setLiked((prev) => ({ ...prev, [id]: !prev[id] }));
+      };
+    
+      function HandleInfo(bk){
+        setInfo(bk);
       }
+      const [sortOrder, setSortOrder]=useState("asc");
+
+
+      const filterData = books.filter((b) =>
+        b.judul.toLowerCase().includes(search.toLowerCase())).sort((a,b)=>{
+          if(sortOrder === "asc"){
+            return a.judul.localeCompare(b.judul);
+          }else if (sortOrder==="desc"){
+            return b.judul.localeCompare(a.judul);
+          }
+          else if (sortOrder==="id-asc"){
+            return a.id-b.id
+          }else if (sortOrder==="pengarang-asc"){
+            return b.Pengarang-a.Pengarang
+          } 
+        }
+      );   
+
       return(
         <div>
-          {books.map((bk) =>(
-            <div key={bk.id}>
-              <h2>{bk.judul}</h2> <HeartIcon />
+          <div className='search'>
+        <input
+          type="text"
+          placeholder="cari disini"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className='search-input'
+        />
+        <Search />
+        <select  className="sort-select" value={sortOrder} onChange={(e)=>setSortOrder(e.target.value)}>
+          <option value="id-asc">urutkan id</option>
+          <option value="asc">urutkan Judul</option>
+          <option value="Pengarang-asc">urutkan pengarang</option>
+
+        </select>
+        <select  className="sort-select" value={sortOrder} onChange={(e)=>setSortOrder(e.target.value)}>
+          <option value="asc">Diurutkan dari A-z</option>
+          <option value="desc">Diurutkan dari Z-A</option>
+        </select>
+      </div>
+      <div className="book-list">
+
+          {filterData.map((bk) =>(
+            <div key={bk.id} className="book">
+              <h2>{bk.judul} <HeartIcon onClick={() => handleLike(bk.id)} className="heart-icon" style={{ fill: liked[bk.id] ? 'red' : 'white' ,alignItems:"right"}}/> </h2> 
               <img src={bk.image} alt="gada" />
               <p>Tahun Terbit : {bk.Tahun_terbit}</p>
-              <button><InfoIcon/> informasi</button>
+              <button  className="info-button" onClick={() => HandleInfo (bk)}><InfoIcon/> informasi</button>
               
             </div>
           ))}
+          {info && (
+            <div className="popup">
+            <h3>{info.judul}</h3>
+            <img src={info.image} alt={info.name} />
+            <p>Pengarang: {info.Pengarang}</p>
+            <p>Penerbit: {info.Penerbit}</p>
+            <p>Tahun Terbit: {info.Tahun_terbit}</p>
+            <p>Halaman: {info.Halaman}</p>
+            <button  onClick={() => setInfo(null)}>Close</button>
+          </div>
+        )}
+        </div>
         </div>
       );
       
